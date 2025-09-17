12f191af39bacad1eb8fd8545df88f0540221208
DimaWebFrontEnd <dmbyndin88@gmail.com>
2025-09-17 16:57:45 +0300

    Working on Responsive Hero Container

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -20,22 +20,22 @@ const Header = () => {
       </figure>
 
       {/* Header Logo/Nav Container */}
-      <section className="flex items-center justify-center w-full z-2 cursor-pointer">
+      <section className="flex items-center justify-between w-full z-2  md:justify-center">
 
         {/* Logo Container */}
-        <section className="absolute left-0 flex items-center justify-between w-33 h-11 px-[11px]">
+        <section className="static left-0 flex items-center w-33 h-11 px-[11px] md:absolute">
           <figure className="w-[25px] h-[25px]">
             <img className="w-full" src={logo} alt="Logo" />
           </figure>
-          <div className="">
+          <div className="ml-2.5">
             <p className="text-base">Backyard</p>
           </div>
         </section>
 
-        {/* Nav Container */}
-        <nav className="bg-[#F3F4F6] p-[3px] flex items-center w-114 h-11 rounded-3xl border-1 border-[#E1E1E275]">
-          <ul className="flex justify-between items-center w-full text-[13px] text-[#AEB0B1] ">
-            <li className="bg-[#303030] text-white px-4 py-2 rounded-3xl">Home</li>
+        {/* Navbar Container */}
+        <nav className="hidden bg-[#F3F4F6] p-[3px] items-center w-97 h-10 rounded-3xl border-1 border-[#E1E1E275] cursor-pointer  md:w-114 md:h-11 sm:flex">
+          <ul className="flex justify-between items-center w-full text-[11px] text-[#AEB0B1] md:text-[13px]">
+            <li className="flex justify-center items-center bg-[#303030] text-white h-7.75 px-4 rounded-3xl md:h-9.5">Home</li>
             <li>About</li>
             <li>How it works</li>
             <li>Token design</li>
@@ -43,13 +43,20 @@ const Header = () => {
           </ul>
         </nav>
 
-        {/* Connect Button (Enable Only If Screen < 1024px)*/}
-        <button className="flex justify-center items-center w-19 h-8 bg-[#303030] text-[13px] text-[#FBFBFC] rounded-full">
+        {/* Connect Button (Enable Only If Screen < 768px)*/}
+        <button className="hidden justify-center items-center w-18.75 h-7.75 ml-2.5 bg-[#303030] text-[11px] text-[#FBFBFC] rounded-full cursor-pointer sm:max-md:flex "> {/* md:hidden */}
           Connect
         </button>
 
         {/* Menu Burger */}
+        <button className="flex items-center justify-center min-w-10 h-10 bg-[#F6F7F8] border-1 border-[#DEDEE373] rounded-xl cursor-pointer sm:hidden">
+          <div className="flex flex-col justify-between items-center w-3.75 h-3">
+            <div className="w-full h-0.5 bg-[#525352]"></div>
+            <div className="w-2.25 h-0.5 bg-[#525352]"></div>
+            <div className="w-full h-0.5 bg-[#525352]"></div>
+          </div>
 
+        </button>
 
       </section>
     </header>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,24 +2,24 @@ import big_logo from "../assets/bigger_logo.svg"
 
 const Heading = () => {
   return (
-    <section className="flex flex-col justify-between mt-20 h-121 z-3">
+    <section className="flex flex-col justify-between mt-20  h-151.75 z-3 sm:h-71.25 lg:h-102 xl:h-121"> {/* h-121 */}
 
       {/* Heading */}
       <div className="flex justify-between items-center">
-         <div className="w-full max-w-256.75">
+         <div className="w-full lg:min-w-185 lg:max-w-185 xl:min-w-235.75 xl:max-w-235.75">
             <h1 className="text-5xl text-[#303030] text-semibold saturate-100 lg:text-7xl xl:text-8xl">
                The yield-backed <br/> 
                stablecoin protocol <br/>
                boosting DeFi liquidity
             </h1>
-            <h3 className="text-[#2626268C] text-[24px] pt-6">
+            <h3 className="text-[#2626268C] text-xl pt-6 saturate-100 xl:text-2xl lg:text-xl  sm:text-base">
                Keep your yield working for you — we keep your liquidity accessible
             </h3>
          </div>
 
          {/* Logo Large */}
-         <figure className="flex items-center justify-center pr-6 pl-6 ">
-            <img className="saturate-100" src={big_logo} alt="Logo Large" />
+         <figure className="flex  items-center justify-center pr-6 pl-6 md:max-w-38 lg:max-w-53 xl:max-w-76">
+            <img className="w-full saturate-100" src={big_logo} alt="Logo Large" />
          </figure>
       </div>
       <div className="flex justify-between w-140">
